6d774c3753e9caa091537effddb35896c0d1f4f1
lea-dubois1 <lea.dubois@laplateforme.io>
2023-09-27 14:14:54 +0200

    gestion des erreurs mise à jour

--- MazeGenerator/src/main/java/org/example/Main.java
+++ MazeGenerator/src/main/java/org/example/Main.java
@@ -14,8 +14,12 @@ public class Main {
             hauteur = Integer.parseInt(args[1]);
         }
         catch (NumberFormatException e) {
+            System.out.println("Erreur : Veuillez fournir une largeur et une hauteur valides supérieurs à 5");
+        }
+        finally {
             System.out.println("Erreur inattendue lors de la génération du labyrinthe. Veuillez réessayer.");
         }
+
 
         if (largeur >= 5 && hauteur >= 5 && !Objects.equals(args[2], "") && !args[2].isEmpty() && !Objects.equals(args[3], "") && !args[3].isEmpty() && Objects.equals(type, "imperfect") || Objects.equals(type, "perfect") && Objects.equals(methode, "simple") || Objects.equals(methode, "graph") || Objects.equals(methode, "optimized")) {
 
